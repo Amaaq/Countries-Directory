9141020bbab04d101b91c3680d5be58eb6cb8fd8
Hicham Maaqoul <hmaaqoul@smb.sqli.com>
2023-09-15 14:49:05 +0100

    feat: add vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+const app = createApp(App)
+const vuetify = createVuetify({    
+    components,
+    directives,
+})
+app.use(vuetify)
+app.mount('#app')
